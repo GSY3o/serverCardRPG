ab1e4c2f4d95598ec4edcafea2777c63843ccb6b
GSY3o <jessy30900@gmail.com>
2021-10-29 06:03:29 -0700

    Add project

---
+++ src/main/java/web/ServerCardRpgApplication.java
@@ -0,0 +1,15 @@
+package web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServerCardRpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerCardRpgApplication.class, args);
+	}
+
+}
